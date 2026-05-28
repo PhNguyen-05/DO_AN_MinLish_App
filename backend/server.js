f312e9db50ad030437e8a516aa787b2396e2dc3b
Trần Hồ Phương Nguyên <phng972005@gmail.com>
2026-05-29 00:00:20 +0700

    upload từ vựng

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 const app = express();
+
+app.use('/static', express.static('public'));
+
 app.use(express.json());
 app.use(cors());
 
